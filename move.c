c08bb1e20ad4a634bca5bf4712aba5b1000e5260
Austin Goodman <Agoody77@gmail.com>
2017-03-29 15:49:50 -0500

    Create move.c

---
+++ move.c
@@ -0,0 +1,26 @@
+
+
+
+int turn(double initial_bearing){
+
+  if(initial_bearing < 0){
+    turn_right();
+  }else if{
+    turn_left();
+  }
+
+  return 0;
+
+}
+
+void turn_right(){
+
+  //gpio pwm out to turn servo(move steering right)
+
+}
+
+void turn_left(){
+
+  //gpio pwm out to turn servo(move steering left)
+
+}
